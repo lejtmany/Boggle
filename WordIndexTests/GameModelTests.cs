f65f0cdfe32b5340b4f85587bddb42d5e8ddd0b3
Joseph Lejtman <lejtmany@gmail.com>
2015-06-10 00:26:09 -0400

    renamed tests

---
+++ WordIndexTests/GameModelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordIndexTests
+{
+    [TestClass]
+    public class GameModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ WordIndexTests/WordIndexTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoggleGame;
+using System.Collections.Generic;
+using FluentAssertions;
+using System.IO;
+
+
+namespace WordIndexTests
+{
+    [TestClass]
+    public class WordIndexTests
+    {
+ //       WordIndex wi = new WordIndex(File.ReadAllLines(Directory.GetCurrentDirectory))
+
+        private WordIndex wi = new WordIndex(File.ReadAllLines("WordList.txt"));
+
+        [TestMethod]
+        public void TestSetInitializesRight()
+        {
+        }
+
+        [TestMethod]
+        public void TestGetPossibleWords()
+        {
+            var possibleWords = wi.ListPossibleWords(new HashSet<char>("mace"));
+            possibleWords.ToString();
+        }
+
+    }
+}
